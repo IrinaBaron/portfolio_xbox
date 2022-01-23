be60352e7ce603ceee3e65b0e1af3f70f22ba7f8
Irina Baron <irr.baron@gmail.com>
2022-01-23 23:54:00 +0200

    worked with section hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,10 @@
 document.addEventListener('DOMContentLoaded', (e) => {
 
-  let elems = document.querySelectorAll('.team__item');
-  let names = document.querySelectorAll('.team__wrap');
-
-  elems.forEach(elem => {
-    elem.addEventListener('click', (e) => {
-      elem.classList.toggle('active');
-
-    })
-  })
-
-  document.addEventListener('click', (e) => {
-    // console.log(e.target)
-    if (!e.target.matches('.team__img')) {
-      elems.forEach(elem => {
-        elem.classList.remove('active');
-      })
-    }
-
-    if (e.target.closest('.team__name')) {
-      let parent = e.target.parentElement;
-      parent.parentElement.classList.toggle('active')
-    }
-
-    if (!e.target.closest('.contacts')) {
-      document.querySelector('.contacts__more').classList.remove('show')
-    }
-
-  })
-
-  document.addEventListener('dblclick', (e) => {
-    if (e.target.closest('.contacts__list')) {
-      document.querySelector('.contacts__more').classList.remove('show')
-    }
-  })
 
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
-
 
     // Navigation arrows
     navigation: {
@@ -47,19 +12,62 @@ document.addEventListener('DOMContentLoaded', (e) => {
       prevEl: '.swiper-button-prev',
     },
 
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
   });
 
-  document.querySelector('.contacts__btn').addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector('.contacts__more').classList.toggle('show')
-  })
 
-  // -----------burger-------------
 
-  document.querySelector('.hero-burger').addEventListener('click', () => {
-    document.querySelector('.hero-burger').classList.toggle('active');
-    document.querySelector('.hero__nav').classList.toggle('active');
-  })
+  // let elems = document.querySelectorAll('.team__item');
+  // let names = document.querySelectorAll('.team__wrap');
+
+  // elems.forEach(elem => {
+  //   elem.addEventListener('click', (e) => {
+  //     elem.classList.toggle('active');
+
+  //   })
+  // })
+
+  // document.addEventListener('click', (e) => {
+  //   // console.log(e.target)
+  //   if (!e.target.matches('.team__img')) {
+  //     elems.forEach(elem => {
+  //       elem.classList.remove('active');
+  //     })
+  //   }
+
+  //   if (e.target.closest('.team__name')) {
+  //     let parent = e.target.parentElement;
+  //     parent.parentElement.classList.toggle('active')
+  //   }
+
+  //   if (!e.target.closest('.contacts')) {
+  //     document.querySelector('.contacts__more').classList.remove('show')
+  //   }
+
+  // })
+
+  // document.addEventListener('dblclick', (e) => {
+  //   if (e.target.closest('.contacts__list')) {
+  //     document.querySelector('.contacts__more').classList.remove('show')
+  //   }
+  // })
+
+
+  // document.querySelector('.contacts__btn').addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   document.querySelector('.contacts__more').classList.toggle('show')
+  // })
+
+  // // -----------burger-------------
+
+  // document.querySelector('.hero-burger').addEventListener('click', () => {
+  //   document.querySelector('.hero-burger').classList.toggle('active');
+  //   document.querySelector('.hero__nav').classList.toggle('active');
+  // })
 
   // ------------scroll--------------
   const btnScroll = document.querySelector('.btn__scroll');
